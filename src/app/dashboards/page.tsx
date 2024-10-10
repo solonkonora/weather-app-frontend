f4d779827147d07e1932ec682b5fca9e44ff86fa
solonkonora <nkwadanora@gmail.com>
2024-10-10 08:56:50 +0100

    started with the charts

--- src/app/dashboards/page.tsx
+++ src/app/dashboards/page.tsx
@@ -3,16 +3,9 @@
 import React from 'react';
 import Navbar from '@/src/components/navbar';
 import Operations from '../../components/weatherOperations';
-// import ProtectedRoute from '../../components/ProtectedRoute';
 
 const Dashboard = () => {
   return (
-    // <ProtectedRoute>
-    //   <div className="dashboard-container">
-    //     <h1>Dashboard</h1>
-    //     <Operations />
-    //   </div>
-    // </ProtectedRoute> */}
     <div className="dashboard-container">
       <Navbar />
       <Operations />
---
+++ src/components/barchart.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { useRef, useEffect } from "react";
+import { Chart } from "chart.js/auto";
+
+export default function Barchart(){
+    return (
+        <div>
+
+        </div>
+    )
+}
